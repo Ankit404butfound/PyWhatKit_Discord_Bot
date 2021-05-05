acad1004ff80dec5df7ac6ebc8504a39c74a5d5d
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2021-05-05 13:57:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from google.cloud import bigquery
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/Ankit/Desktop/PyWhatKit TEST/config.json"
+client = bigquery.Client()
+
+query = """
+   SELECT count(*) as Downloads
+FROM `bigquery-public-data.pypi.file_downloads`
+WHERE file.project = 'pywhatkit'
+  -- Only query the last 30 days of history
+  AND DATE(timestamp)
+    BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 0 DAY)
+    AND CURRENT_DATE()
+"""
+query_job = client.query(query)  # Make an API request.
+
+print("The query data:")
+row = [*query_job][0]
+print(row[0])
+
