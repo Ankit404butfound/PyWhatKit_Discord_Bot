92a102b64e430ab7c33b53c0764f8762dabe7965
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2021-05-06 10:05:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ async def send_count():
         channel = bot.get_channel(839422789849317406)
         query_job = client.query(query_1)
         await channel.send("Pywhatkit has been downloaded %s times in last 24 hours."%str([*query_job][0][0]))
-        await asyncio.sleep(10)
+        await asyncio.sleep(86400)
 
 
 @bot.event
