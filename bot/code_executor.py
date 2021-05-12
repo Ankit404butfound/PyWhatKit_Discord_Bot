2be1f769d9c0412d05011bf777820b61916b3c65
Ankit Raj Mahapatra <54436840+Ankit404butfound@users.noreply.github.com>
2021-05-12 08:06:21 +0530

    Update code_executor.py

--- bot/code_executor.py
+++ bot/code_executor.py
@@ -5,7 +5,7 @@ def execute(code):
     file = open("bot/agent.py","w",encoding="utf-8")
     file.write(code)
     file.close()
-    os.system("python bot/executor.py > output.txt")
+    os.system("python bot/executor.py > bot/output.txt")
     data = open("bot/output.txt",encoding="utf-8").read()
     if data != "":
         return data if len(data) <= 2000 else "Output too big, returning first 4000 characters\n"+data[:4000]
