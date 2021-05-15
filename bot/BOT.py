168db67593561c76c279f3966e9e581d39eea8fd
aaryanrr <raiaryan2422@gmail.com>
2021-05-15 13:31:41 +0530

    Updated bot

--- bot/BOT.py
+++ bot/BOT.py
@@ -9,29 +9,25 @@ from my_psql import *
 
 import asyncio
 
+
 class Bot:
     def __init__(self):
         self.client = client
         self.bot = bot
-        
-
 
     def download_notifier():
         send_count()
-        
 
     @bot.event
     async def on_member_join(member):
         await member.guild.system_channel.send(f"""Hello {member.mention} and Thanks for joining PyWhatKit's server.
 Be sure to follow the <#830332694231384074> of this server.
 Please head over to <#830319507360186389> and consider introducing yourself.""")
-       
-       
+
     @bot.event
     async def on_ready():
         print('Bot is ready')
         asyncio.gather(send_count())
-
 
     @bot.event
     async def on_message(message):
@@ -47,40 +43,41 @@ Please head over to <#830319507360186389> and consider introducing yourself.""")
             print("The query data:")
             row = [*query_job][0]
             print(row[0])
-            await message.channel.send("Pywhatkit has been downloaded %s times in time range between 00:00 UTC till now."%str(row[0]))
+            await message.channel.send(
+                "Pywhatkit has been downloaded %s times in time range between 00:00 UTC till now." % str(row[0]))
 
         if ".pgrsql" in message.content:
-            sql = message.content.replace(".pgrsql ","")
+            sql = message.content.replace(".pgrsql ", "")
             roles = message.author.roles
             for role in roles:
-               if role.name in allowed_roles:
-                   mod = True
+                if role.name in allowed_roles:
+                    mod = True
             if mod:
-                await message.channel.send("`"+execute_sql(sql)+"`")
+                await message.channel.send("`" + execute_sql(sql) + "`")
             else:
                 await message.channel.send("`ERROR: You have limited access to this bot`")
 
         if "#add_todo" in message.content:
             roles = message.author.roles
-            task = message.content.replace("#add_todo ","")
+            task = message.content.replace("#add_todo ", "")
             for role in roles:
-               if role.name in allowed_roles:
-                   mod = True
+                if role.name in allowed_roles:
+                    mod = True
             if mod:
                 await message.channel.send(f"`{add_todo(message.author.id, task)}`")
 
         if "#what_todo" in message.content:
             roles = message.author.roles
             for role in roles:
-               if role.name in allowed_roles:
-                   mod = True
+                if role.name in allowed_roles:
+                    mod = True
 
             if mod:
                 tasks = ""
                 dbs = fetch_todo(message.author.id)
                 for task in dbs:
                     tasks = f"{tasks}Task_id_{task[0]}. {task[1]}\n"
-                    
+
                 if tasks != "":
                     await message.channel.send(f"`Here's the list of your pending tasks\n{tasks}`")
                 else:
@@ -88,10 +85,10 @@ Please head over to <#830319507360186389> and consider introducing yourself.""")
 
         if "#todo_done" in message.content:
             roles = message.author.roles
-            task_id = message.content.replace("#todo_done ","")
+            task_id = message.content.replace("#todo_done ", "")
             for role in roles:
-               if role.name in allowed_roles:
-                   mod = True
+                if role.name in allowed_roles:
+                    mod = True
             if mod:
                 if todo_done(task_id):
                     await message.channel.send(f"`Done`")
@@ -100,17 +97,17 @@ Please head over to <#830319507360186389> and consider introducing yourself.""")
 
         if "#todo_log" in message.content:
             roles = message.author.roles
-            user_id = message.content.replace("#todo_log","").strip()
+            user_id = message.content.replace("#todo_log", "").strip()
             for role in roles:
-               if role.name in allowed_roles:
-                   mod = True
+                if role.name in allowed_roles:
+                    mod = True
             if mod:
                 if user_id == "":
                     user_id = message.author.id
                 log_message = ""
                 user_log = log(user_id)
                 print(user_log)
-                
+
                 for data in user_log:
                     if data[2] == "p":
                         status = "Pending"
@@ -126,11 +123,11 @@ Please head over to <#830319507360186389> and consider introducing yourself.""")
             try:
                 data = requests.get("https://www.generatormix.com/random-facts-generator").content
                 soup = bs4(data)
-                fact = soup.find("blockquote",attrs = {'class':"text-left"})
-         # print((fact.text))
+                fact = soup.find("blockquote", attrs={'class': "text-left"})
+                # print((fact.text))
                 await message.channel.send(fact.text)
             except Exception as e:
                 await message.channel.send(str(e))
-            
+
     def start(self):
         self.bot.run(token)
