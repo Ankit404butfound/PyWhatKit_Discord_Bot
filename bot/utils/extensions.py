ec230074e2fe5996ac477e8ef6aae4d73fb8f2b3
aaryanrr <raiaryan2422@gmail.com>
2021-08-11 13:26:16 +0530

    Load Extensions/Cogs in the Bot

---
+++ bot/utils/extensions.py
@@ -0,0 +1,32 @@
+import importlib
+import inspect
+import pkgutil
+from typing import Iterator
+
+import cogs
+
+
+def unqualify(name: str) -> str:
+    """Return an Unqualified name given a Qualified module/package Name"""
+    return name.rsplit(".", maxsplit=1)[-1]
+
+
+def walk_extensions() -> Iterator[str]:
+    """Get Extension Names from bot.cogs Subpackage"""
+
+    def on_error(name: str) -> None:
+        raise ImportError(name=name)
+
+    for module in pkgutil.walk_packages(cogs.__path__, f"{cogs.__name__}.", onerror=on_error):
+        if unqualify(module.name).startswith("_"):
+            continue
+
+        if module.ispkg:
+            imported = importlib.import_module(module.name)
+            if not inspect.isfunction(getattr(imported, "setup", None)):
+                continue
+
+        yield module.name
+
+
+EXTENSIONS = frozenset(walk_extensions())
